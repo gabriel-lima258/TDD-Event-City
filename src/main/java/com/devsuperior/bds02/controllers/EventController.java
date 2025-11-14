d6b34e0c7e3e0434a4e07c35f289c261fd3e5355
Gabriel Lima <gabriel58221@gmail.com>
2025-11-14 00:06:04 -0300

    first commit

---
+++ src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.bds02.controllers;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.services.CityService;
+import com.devsuperior.bds02.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+}
